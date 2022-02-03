021c96076220946088dfb3616a373d0453730780
PaulThomas20002 <paulthomas20002@gmail.com>
2022-02-03 17:36:38 +0530

    File of Cycle 2 is uploaded

--- Cycle-2/employee.java
+++ Cycle-2/employee.java
@@ -4,40 +4,62 @@ It also has a method to print salary which prints the salary which prints the sa
 Now assign name , age, Phone number, address and salary to an officer and a manager .
 by making an object of both of this classes and print the same.*/
 
-class employee{
+class Employee{
+    int id;
     String name;
-    int age;
+    String dob;
     double phonnum;
     String address;
     double salary;
-    
-    void printall(){
-        System.out.println("Name : "+ name+"\nAge : "+ age+"\nPhone Number : "+phonnum+"\nAddress : "+address+"\nSalary : "+salary);
-    }
+  
     void printsalary(){
         System.out.println(name+" "+salary);
     }
 }
-class officer extends employee{
+class officer extends Employee{
     String department;
     String specialization;
     
+    void printall(){
+        System.out.println("\nID : "+id+"\nName : "+ name+"\nDOB : "+dob+"\nPhone Number : "+phonnum+"\nAddress : "+address+"\nSalary : "+salary+"\nSpecialization : "+specialization+"\nDepartment : "+department);
+    }
+    
 }
-class manager extends employee{
+class manager extends Employee{
     String department;
-    String specialization;   
+    String specialization; 
+    
+    void printall(){
+        System.out.println("\nID : "+id+"\nName : "+ name+"\nDOB : "+dob+"\nPhone Number : "+phonnum+"\nAddress : "+address+"\nSalary : "+salary+"\nSpecialization : "+specialization+"\nDepartment : "+department);
+    }  
 }
 
 class employee{
     public static void main(String []args){
         officer o = new officer();
-        o.name="Paul Thomas";
-        o.age = 19;
+        o.id = 369;
+        o.name="Paul";
+        o.dob = "25.04.2002";
         o.phonnum = 200;
-        o.salary = 4334;
+        o.salary = 43344;
         o.address = "India";
+        o.specialization = "Machine Learning";
+        o.department = "Computer Science";
         o.printsalary();
-        System.out.println("More Details");
+        
+        o.printall();
+        
+        manager b = new manager();
+        o.id = 555;
+        o.name="Thomas";
+        o.dob = "26.05.2002";
+        o.phonnum = 300;
+        o.salary = 55344;
+        o.address = "Australia";
+        o.specialization = "Block Chain";
+        o.department = "Computer Science";
+        o.printsalary();
+        
         o.printall();
     }
 }
